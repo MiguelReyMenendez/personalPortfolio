20684cbfb30bbb8a08f85d4507f019da900efa16
Miguel Rey <miguelreymenendez@gmail.com>
2023-07-16 23:12:44 +0200

    template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            fontFamiliy: {
+                roboto: ['Roboto', 'sans-serif'],
+            },
+            animation: {
+                text: 'text 5s ease infinite',
+              },
+              keyframes: {
+                text: {
+                  '0%, 100%': {
+                    'background-size': '200% 200%',
+                    'background-position': 'left center',
+                  },
+                  '50%': {
+                    'background-size': '200% 200%',
+                    'background-position': 'right center',
+                  },
+                },
+            },
+        },
 	},
 	plugins: [
         require('tailwindcss-fluid-type')({
